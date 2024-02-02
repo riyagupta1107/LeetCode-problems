028475c84a35c8606d0ab3841234abf299bad4ab
riyagupta1107 <riya.gupta070705@gmail.com>
2024-02-02 14:56:38 +0530

    Update lc-1-two-sum.py

--- Easy level problems/lc-1-two-sum.py
+++ Easy level problems/lc-1-two-sum.py
@@ -2,7 +2,7 @@
 #https://leetcode.com/problems/two-sum/
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         for i in range(0,len(nums)):
             sum=nums[i]
             for j in range(i+1,len(nums)):
