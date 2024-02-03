af69f0e4292dedb070fe942f4b2b2f3a8285d6d4
riyagupta1107 <riya.gupta070705@gmail.com>
2024-02-03 13:09:02 +0530

    Create lc-15-3sum.py

---
+++ Medium level problems/lc-15-3sum.py
@@ -0,0 +1,21 @@
+#My initial solution to the problem
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        outer=list()
+        sum_outer=0
+        for i in range(0,len(nums)):
+            sum_outer+=nums[i]
+            sum_inner=0
+            for j in range(i+1,len(nums)):
+                sum_inner+=nums[j]
+                for k in range(j+1,len(nums)):
+                    sum_inner+=nums[k]
+                    if nums[i]+nums[j]+nums[k]==0:
+                        outer.append([nums[i],nums[j],nums[k]])
+                sum_inner=nums[i]
+        if outer!=None:
+            return outer
+        else:
+            return []
+        
